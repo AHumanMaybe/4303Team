a91ef477665b732e54554b568906ba9d21dea1ce
Evan Hess <enhcode@gmail.com>
2019-09-21 16:58:31 -0400

    Updated code on 4303Prog files, especially 'main.cpp' (src), and added vision.h (include).

--- programming/4303Prog/include/vex.h
+++ programming/4303Prog/include/vex.h
@@ -15,5 +15,21 @@
 #include "v5.h"
 #include "v5_vcs.h"
 
-vex::brain       Brain;
-vex::competition Competition;
+vex::brain Brain = vex::brain();
+vex::competition Competition = vex::competition();
+
+vex::controller Controller = vex::controller();
+
+vex::motor A = vex::motor(vex::PORT1);
+vex::motor B = vex::motor(vex::PORT5, true);
+vex::motor HD = vex::motor(vex::PORT11);
+
+vex::motor CLAW = vex::motor(vex::PORT15);
+vex::motor L1 = vex::motor(vex::PORT16);
+vex::motor L2 = vex::motor(vex::PORT16);
+vex::motor R1 = vex::motor(vex::PORT16, true);
+vex::motor R2 = vex::motor(vex::PORT20, true);
+
+vex::pot AUTO = vex::pot(Brain.ThreeWirePort.A);
+vex::limit LIMIT = vex::limit(Brain.ThreeWirePort.D);
+vex::line LINE = vex::line(Brain.ThreeWirePort.H);
--- programming/4303Prog/src/main.cpp
+++ programming/4303Prog/src/main.cpp
@@ -7,6 +7,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 #include "vex.h"
+#include "vision.h"
 
 using namespace vex;
 
@@ -22,21 +23,77 @@ void autonomous(void)
 
 void usercontrol(void) 
 {
-  while(1)
-  {
+  HD.setVelocity(75, percentUnits::pct);   //set speeds for h-drive
+  L1.setVelocity(50, percentUnits::pct);   //set speeds for arm
+  L2.setVelocity(50, percentUnits::pct);
+  R1.setVelocity(50, percentUnits::pct);
+  R2.setVelocity(50, percentUnits::pct);
+  CLAW.setVelocity(50, percentUnits::pct); //set speeds for claw
 
+  while(true)
+  {
+    A.spin(directionType::fwd, Controller.Axis3.position(), percentUnits::pct); //tank drive left stick
+    B.spin(directionType::fwd, Controller.Axis2.position(), percentUnits::pct); //tank drive right stick
+    
+    if(Controller.ButtonLeft.pressing()) //h-drive
+    {
+      HD.spin(directionType::fwd);
+    }
+    else if(Controller.ButtonRight.pressing())
+    {
+      HD.spin(directionType::rev);
+    }
+    else 
+    {
+      HD.stop(brakeType::hold);
+    }
+
+    if(Controller.ButtonR1.pressing()) //claw control
+    {
+      CLAW.spin(directionType::fwd);
+    }
+    else if(Controller.ButtonR2.pressing())
+    {
+      CLAW.spin(directionType::fwd);
+    }
+    else
+    {
+      CLAW.stop(brakeType::hold);
+    }
+
+    if(Controller.ButtonUp.pressing()) //arm raise
+    {
+      L1.spin(directionType::fwd);
+      L2.spin(directionType::fwd);
+      R1.spin(directionType::fwd);
+      R2.spin(directionType::fwd);
+    }
+    else if(Controller.ButtonDown.pressing())
+    {
+      L1.spin(directionType::fwd);
+      L2.spin(directionType::fwd);
+      R1.spin(directionType::fwd);
+      R2.spin(directionType::fwd);
+    }
+    else
+    {
+      L1.stop(brakeType::hold);
+      L2.stop(brakeType::hold);
+      R1.stop(brakeType::hold);
+      R2.stop(brakeType::hold);
+    }
   }
 }
 
 int main() 
 {
-    Competition.autonomous( autonomous );
-    Competition.drivercontrol( usercontrol );
+  Competition.autonomous( autonomous );
+  Competition.drivercontrol( usercontrol );
     
-    pre_auton();
+  pre_auton();
        
-    while(1) 
-    {
-      vex::task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
-    }
+  while(1) 
+  {
+    vex::task::sleep(100); //Sleep the task for a short amount of time to prevent wasted resources.
+  }
 }
