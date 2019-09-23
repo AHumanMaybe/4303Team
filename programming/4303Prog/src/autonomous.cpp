39d2392cc4a8db8af406c03cdf93aac4da213f49
enh-code <55031410+enh-code@users.noreply.github.com>
2019-09-23 08:58:44 -0400

    Add files via upload
    Started autonomous and added functionality.

--- programming/4303Prog/src/autonomous.cpp
+++ programming/4303Prog/src/autonomous.cpp
@@ -4,11 +4,15 @@
 using namespace vex;
 
 //dark = high, light - low
-long int lightLevel;
+long int lightlevel;
+long int potvalue;
 vision::signature colors[] = {ORANGE, GREEN, PURPLE};
 
 //=============================================================================
 //define ALL MOTORS AND SENSORS
+
+extern brain Brain;
+extern controller Controller;
 
 //BASE
 extern motor A;
@@ -31,21 +35,77 @@ extern gyro GYRO; //degrees of rotation * 10 = gyro value (long/int32_t)
 
 void pre_auton(void)
 {
-  lightLevel = LINE.value(analogUnits::range12bit); //0 (shining light) to 4095 (pitch black)
+  lightlevel = LINE.value(analogUnits::range12bit); //0 (shining light) to 4095 (pitch black)
+  potvalue = POT.value(analogUnits::range12bit); //0 to 4095
   GYRO.startCalibration(1300);
 }
 
 void autonomous(void) 
 {
   short c = 1;
+  vision::object *cube;
   VS.takeSnapshot(ORANGE);
   
   find:
-  while(!VS.largestObject.exists && VS.largestObject.width > 15)
+  Controller.Screen.clearScreen();
+  Controller.Screen.setCursor(1, 1);
+  Controller.Screen.print("Looking for object...");
+
+  while(!VS.largestObject.exists || !(VS.largestObject.width > 15)) //resolution is 316x212
   {
     VS.takeSnapshot(colors[c]);
     c++;
+
+    c = (c > 2) ? 0 : c;
+
+    if(potvalue > 2047) //scan right
+    {
+      HD.spin(directionType::rev, 75, percentUnits::pct);
+    }
+    else if(potvalue <= 2047)
+    {
+      HD.spin(directionType::fwd, 75, percentUnits::pct);
+    }
   }
 
-  //obj:
+  Controller.Screen.clearScreen();
+  Controller.Screen.setCursor(1, 1);
+  Controller.Screen.print("Object found!");
+
+  c = (c == 0) ? 2 : c--; //set c to 2 if c == 0
+
+  cube = &VS.largestObject;
+
+  Controller.Screen.newLine();
+  Controller.Screen.print("Moving...");
+
+  //find cube position and move to it
+  while(cube->centerX != 157)
+  {
+    if(cube->centerX < 157) //turn right
+    {
+      HD.spin(directionType::rev, 75, percentUnits::pct);
+    }
+    else if(cube->centerX > 157) //turn left
+    {
+      HD.spin(directionType::fwd, 75, percentUnits::pct);
+    }
+
+    VS.takeSnapshot(colors[c]);
+    cube = &VS.largestObject;
+
+    if(!cube->exists || !(cube->width > 15))
+    {
+      Controller.Screen.clearScreen();
+      Controller.Screen.setCursor(1, 1);
+      Controller.Screen.print("Object lost.");
+      Controller.Screen.newLine();
+      Controller.Screen.print("Searching...");
+      goto find;
+    }
+  }
+
+  HD.stop(brakeType::brake);
+
+  //TODO: move toward and grab cube
 }
