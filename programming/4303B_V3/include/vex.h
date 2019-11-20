40b8fe3bfb858a994d84cac34d169b325df45bd0
enh-code <55031410+enh-code@users.noreply.github.com>
2019-11-20 10:40:50 -0500

    Changed ports

--- programming/4303B_V3/include/vex.h
+++ programming/4303B_V3/include/vex.h
@@ -21,8 +21,8 @@ vex::controller Controller = vex::controller();
 vex::gyro GYRO(Brain.ThreeWirePort.A);
 vex::limit LIMIT(Brain.ThreeWirePort.H);
 
-vex::motor b_left(vex::PORT1);
-vex::motor b_right(vex::PORT5, true);
+vex::motor b_left(vex::PORT13);
+vex::motor b_right(vex::PORT17, true);
 vex::motor hdrive(vex::PORT6);
 vex::smartdrive base(b_left, b_right, GYRO, 319.19, 355.6, 130, vex::distanceUnits::mm);
 
@@ -32,9 +32,9 @@ vex::motor r_arm(vex::PORT15);
 vex::motor_group arm(l_arm, r_arm);
 
 //top claw (l1, l2) [1 is open]
-vex::motor top(vex::PORT16);
+vex::motor top(vex::PORT2);
 //bottom claw (x, b) [x is open]
-vex::motor bottom(vex::PORT20);
+vex::motor bottom(vex::PORT4);
 
 #define waitUntil(condition)                                                   \
   do {                                                                         \
